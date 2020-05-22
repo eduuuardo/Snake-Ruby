0d6cd8172db40f5eac78db4a932c532a4ada2e4c
eduuuardo <dnb2809@gmail.com>
2020-05-22 11:06:11 -0500

    Se agregan pruebas, acciones pa mover la vibora y pa que la comida salga en lugares random

--- src/app.rb
+++ src/app.rb
@@ -3,25 +3,39 @@ require_relative "model/state"
 require_relative "actions/actions"
 
 class App
-    def initialize
-        @state = Model::initial_state
-    end
+  def initialize
+    @state = Model::initial_state
+  end
 
-    def start
-        view = View::Ruby2dView.new
-        Thread.new { init_timer(view) }
-        view.start(@state)
-    end
+  def start
+    @view = View::Ruby2dView.new(self)
+    timer_thread = Thread.new { init_timer }
+    @view.start(@state)
+    timer_thread.join
+  end
 
-    def init_timer(view)
-        loop do
-            @state = Actions::move_snake(@state)
-            view.render(@state)
-            sleep 0.5
-        end
+  def init_timer
+    loop do
+      if @state.game_finished
+        puts "Juego finalizado"
+        puts "Puntaje: #{@state.snake.positions.length}"
+        break
+      end
+      @state = Actions::move_snake(@state)
+      @view.render(@state)
+      sleep 0.5
     end
+  end
+
+  def send_action(action, params)
+    # :change_direction, Model::Direction::UP
+    new_state = Actions.send(action, @state, params)
+    if new_state.hash != @state.hash
+      @state = new_state
+      @view.render(@state)
+    end
+  end
 end
-
 
 app = App.new
 app.start
--- tests/actions_test.rb
+++ tests/actions_test.rb
@@ -3,15 +3,16 @@ require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
 class ActionsTest < Minitest::Test
+
     def setup
         @initial_state = Model::State.new(
             Model::Snake.new([
-                Model::Coord.new(1,1),
+                Model::Coord.new(2,1),
                 Model::Coord.new(1,0)
             ]),
             Model::Food.new(4,4),
             Model::Grid.new(8,12),
-            Model::Direction::RIGHT,
+            Model::Direction::DOWN,
             false
         )
     end
@@ -19,12 +20,12 @@ class ActionsTest < Minitest::Test
     def test_move_snake
         expected_state = Model::State.new(
             Model::Snake.new([
-                Model::Coord.new(2,1),
+                Model::Coord.new(1,1),
                 Model::Coord.new(1,1)
             ]),
             Model::Food.new(4,4),
             Model::Grid.new(8,12),
-            Model::Direction::RIGHT,
+            Model::Direction::DOWN,
             false
         )
 
@@ -40,25 +41,25 @@ class ActionsTest < Minitest::Test
             ]),
             Model::Food.new(4,4),
             Model::Grid.new(8,12),
-            Model::Direction::RIGHT,
+            Model::Direction::DOWN,
             false
         )
-        actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
         assert_equal actual_state, expected_state
     end
 
     def test_change_direction_valid
         expected_state = Model::State.new(
             Model::Snake.new([
-                Model::Coord.new(2,1),
-                Model::Coord.new(1,1)
+                Model::Coord.new(1,1),
+                Model::Coord.new(0,1)
             ]),
             Model::Food.new(4,4),
             Model::Grid.new(8,12),
-            Model::Direction::RIGHT,
+            Model::Direction::LEFT,
             false
         )
-        actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
         assert_equal actual_state, expected_state
     end
 end
